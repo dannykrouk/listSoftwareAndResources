3a741ffff9fa06a4808e63885fce22b805eef4af
Danny Krouk <dkrouk@esri.com>
2022-04-26 11:22:28 -0700

    Update list_software_and_resources.ps1

--- list_software_and_resources.ps1
+++ list_software_and_resources.ps1
@@ -1,4 +1,4 @@
-# Exports a list of software installed, processor information, and memory information on the target machine
+# Exports a list of software installed, processor information, memory information, ODBC drivers, and ODBC Dsns on the target machine
 # (default is localhost)
 #
 # Usage Example
@@ -8,10 +8,22 @@
 # Should be run as the machine administrator ("as administrator")
 
 param	(
-		[Parameter(Position=0)][string]$machine="localhost"
+		[Parameter(Position=0)][string]$machine="dannyk.esri.com"
 		)
+
 
 Get-WmiObject -ComputerName $machine -Class Win32_Product | select-object -property name,version | Export-Csv "$($machine)_installed_software.csv" -NoTypeInformation
 Get-WmiObject -ComputerName $machine -Class Win32_Processor | select-object -property name,numberofcores,numberoflogicalprocessors | Export-Csv "$($machine)_processors.csv"  -NoTypeInformation
 Get-WmiObject -ComputerName $machine -Class Win32_PhysicalMemory | select-object -property name,capacity | Export-Csv "$($machine)_memory.csv" -NoTypeInformation
 
+if ($machine -eq "localhost")
+{
+    Get-OdbcDriver | select-object -Property Name, Platform | Export-Csv "$($machine)_odbcdrivers.csv" -NoTypeInformation
+    Get-OdbcDsn | select-object -Property DriverName, DsnType, Name, Platform | Export-Csv "$($machine)_odbcdsns.csv" -NoTypeInformation
+}
+else
+{
+    $sess = New-CimSession -ComputerName $machine
+    Get-OdbcDriver -CimSession $sess  | select-object -Property Name, Platform | Export-Csv "$($machine)_odbcdrivers.csv" -NoTypeInformation
+    Get-OdbcDsn -CimSession $sess  | select-object -Property DriverName, DsnType, Name, Platform | Export-Csv "$($machine)_odbcdsns.csv" -NoTypeInformation
+}
